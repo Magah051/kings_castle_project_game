1ec88f921f139d4de71a9201bb112e70141bcb1a
José Magalhães <joses.magalhaes@hotmail.com>
2023-07-31 00:21:22 -0300

    Colisão de blocos e remoção de ponto e vírgula para padronização

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,35 +1,36 @@
 class Player {
-    constructor(){
-        this.position = {
-            x: 100,
-            y: 100
-        }
-
-        this.velocity = {
-            x: 0,
-            y: 0
-        }
-
-        this.width = 100;
-        this.height = 100;
-        this.sides = {
-            bottom: this.position.y + this.height
-        }
-        this.gravity = 1;
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
     }
 
-    draw(){
-        c.fillStyle = 'red';
-        c.fillRect(this.position.x,this.position.y,this.width,this.height);
+    this.velocity = {
+      x: 0,
+      y: 0,
     }
 
-    update(){
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-    this.sides.bottom = this.position.y + this.height;
+    this.width = 100
+    this.height = 100
+    this.sides = {
+      bottom: this.position.y + this.height,
+    }
+    this.gravity = 1
+  }
 
-    if (this.sides.bottom + this.velocity.y < canvas.height){
-        this.velocity.y += this.gravity;
-    } else this.velocity.y = 0;
+  draw() {
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+
+  update() {
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
+    this.sides.bottom = this.position.y + this.height
+
+    // above bottom of canvas
+    if (this.sides.bottom + this.velocity.y < canvas.height) {
+      this.velocity.y += this.gravity
+    } else this.velocity.y = 0
   }
 }
